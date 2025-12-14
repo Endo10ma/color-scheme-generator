11f656e1b36be34da65391e3a91e1552e3ac1cd5
Endo10ma <154909934+Endo10ma@users.noreply.github.com>
2025-12-14 23:02:00 +0800

    completed the script(and the project)

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&mode=quad&count=5&format=json')
- .then(response => response.json())
- .then(data=> {
-    console.log(data.colors)
-})
-
 const color1 = document.getElementById('color1')
 const color2 = document.getElementById('color2')
 const color3 = document.getElementById('color3')
@@ -11,6 +5,7 @@ const color4 = document.getElementById('color4')
 const color5 = document.getElementById('color5')
 const btn = document.getElementById('submitBtn')
 const colorSelection = document.getElementById('color')
+const colorScheme = document.getElementById('color-scheme')
 
 function changeColor(element, color){
     element.firstElementChild.innerHTML = `<p>${color}</p`
@@ -19,11 +14,15 @@ function changeColor(element, color){
 
 
 btn.addEventListener('click', () => {
-    console.log(colorSelection.value)
 
-fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelection.value.slice(1)}&mode=quad&count=5&format=json`)
+fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelection.value.slice(1)}&mode=${colorScheme.value}&count=5`)
  .then(response => response.json())
  .then(data=> {
     console.log(data.colors)
+
+    data.colors.forEach((element, index) => {
+        const target = document.getElementById(`color${index+1}`)
+        changeColor( target, element.hex.value)
+    });
 })
 })
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,23 @@
+const color1 = document.getElementById('color1')
+// const color2 = document.getElementById('color2')
+// const color3 = document.getElementById('color3')
+// const color4 = document.getElementById('color4')
+// const color5 = document.getElementById('color5')
+// const btn = document.getElementById('submitBtn')
+// const colorSelection = document.getElementById('color')
+
+// function changeColor(element, color){
+//     element.firstElementChild.innerHTML = `<p>${color}</p`
+//     element.style.backgroundColor = `${color}`
+// }
+
+
+// btn.addEventListener('click', () => {
+//     console.log(colorSelection.value)
+
+// fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelection.value.slice(1)}&mode=quad&count=5&format=json`)
+//  .then(response => response.json())
+//  .then(data=> {
+//     console.log(data.colors)
+// })
+// })
