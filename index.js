71257a9d9e32b83ab12a693fd06f3ba6c82bc362
Endo10ma <154909934+Endo10ma@users.noreply.github.com>
2025-12-14 22:32:14 +0800

    finished the styling

---
+++ index.js
@@ -0,0 +1,29 @@
+fetch('https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&mode=quad&count=5&format=json')
+ .then(response => response.json())
+ .then(data=> {
+    console.log(data.colors)
+})
+
+const color1 = document.getElementById('color1')
+const color2 = document.getElementById('color2')
+const color3 = document.getElementById('color3')
+const color4 = document.getElementById('color4')
+const color5 = document.getElementById('color5')
+const btn = document.getElementById('submitBtn')
+const colorSelection = document.getElementById('color')
+
+function changeColor(element, color){
+    element.firstElementChild.innerHTML = `<p>${color}</p`
+    element.style.backgroundColor = `${color}`
+}
+
+
+btn.addEventListener('click', () => {
+    console.log(colorSelection.value)
+
+fetch(`https://www.thecolorapi.com/scheme?hex=${colorSelection.value.slice(1)}&mode=quad&count=5&format=json`)
+ .then(response => response.json())
+ .then(data=> {
+    console.log(data.colors)
+})
+})
